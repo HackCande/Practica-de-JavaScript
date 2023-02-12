0f2bd922d14d9f7647522d70602eb798992259f4
HackCande <csh123123csh@gmail.com>
2023-02-11 21:26:13 -0600

    Menu de Navegacion

---
+++ Ejercicio_1.js
@@ -0,0 +1,7 @@
+// La funcion Console.log(), le permite al usuario mostrar el contenido, esto tambien sirve para ver errores y asi corregirlos.
+
+let valusuario = parseInt(prompt("Ingrese numero: "));
+let oper = parseInt(prompt("Ingrese un multiplo: "));
+
+let result = valusuario * oper;
+console.log("El resultado es: ",result);
